6b0847406c12b1ea2e8a1f5c5805c03a9f170057
Hiura <robinaska444@gmail.com>
2026-08-17 03:45:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': {
@@ -13,5 +16,3 @@ export default defineConfig({
     }
   }
 })
-
-
